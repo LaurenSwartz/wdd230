0cd7e3e794c83d4b9d5962a00898b30cbaedc748
LaurenSwartz <swa21010@byui.edu>
2023-10-24 23:53:44 +0200

    NEW CHANGES

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url ='https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=-6.64&appid=7a4bfb22f4e1329c7356ba7471d3389d&units=imperial'
+const url ='https://api.openweathermap.org/data/2.5/weather?q=roodepoort&appid=7a4bfb22f4e1329c7356ba7471d3389d&units=metric'
 
 async function apiFetch(){
     try {
