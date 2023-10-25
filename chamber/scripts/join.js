e4106e795083a82526e9e0819b92272eede414f2
LaurenSwartz <swa21010@byui.edu>
2023-10-25 08:10:13 +0200

    new

--- chamber/scripts/join.js
+++
@@ -1,8 +0,0 @@
-timeStamp = document.getElementById("timestamp");
-
-timeStamp.value = Date.now();
-
-document.getElementById("formmessage").addEventListener("submit", function (event) {
-    event.preventDefault();
-    window.location.href = "thankyou.html";
-});
